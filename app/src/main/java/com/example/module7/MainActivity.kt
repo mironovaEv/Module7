b9a6ad8d701435ab72d5ae26f9e62d31e3149102
mironovaEv <mironovaev26@gmail.com>
2022-05-14 11:11:49 +0700

    recyclerView

--- app/src/main/java/com/example/module7/MainActivity.kt
+++ app/src/main/java/com/example/module7/MainActivity.kt
@@ -1,29 +1,89 @@
 package com.example.module7
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ScrollView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.*
+import androidx.recyclerview.widget.RecyclerView
 import com.example.module7.Model.AlmostVirtualMachine
+import com.example.module7.Model.RecyclerviewAdapter
 import com.example.module7.databinding.ActivityMainBinding
-import android.view.View.OnClickListener
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+    lateinit var recyclerviewAdapter: RecyclerviewAdapter
 
+    private val itemTouchHelper by lazy {
+        val itemTouchCallback =
+            object : ItemTouchHelper.SimpleCallback(UP or DOWN or START or END, RIGHT) {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    val recyclerviewAdapter = recyclerView.adapter as RecyclerviewAdapter
+                    val fromPosition = viewHolder.adapterPosition
+                    val toPosition = target.adapterPosition
+                    recyclerviewAdapter.moveItem(fromPosition, toPosition)
+
+                    return true
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val position = viewHolder.adapterPosition
+                    recyclerviewAdapter.onItemDismiss(position);
+
+
+                }
+
+                override fun onSelectedChanged(
+                    viewHolder: RecyclerView.ViewHolder?,
+                    actionState: Int
+                ) {
+                    super.onSelectedChanged(viewHolder, actionState)
+                    if (actionState == ACTION_STATE_DRAG) {
+                        viewHolder?.itemView?.scaleY = 1.3f
+                        viewHolder?.itemView?.alpha = 0.7f
+
+                    }
+                }
+
+                override fun clearView(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder
+                ) {
+                    super.clearView(recyclerView, viewHolder)
+                    viewHolder.itemView.scaleY = 1.0f
+                    viewHolder.itemView.alpha = 1.0f
+                }
+
+            }
+        ItemTouchHelper(itemTouchCallback)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        init()
+        itemTouchHelper.attachToRecyclerView(binding.rcView)
+        recyclerviewAdapter = RecyclerviewAdapter()
+        binding.rcView.adapter = recyclerviewAdapter
+
         val outCloseBtn = findViewById<ImageButton>(R.id.imageButton)
         outCloseBtn.setOnClickListener {
             findViewById<ScrollView>(R.id.scrollView).visibility = View.GONE
             outCloseBtn.visibility = View.GONE
         }
+
         binding.buttonRun.setOnClickListener {
             val program = AlmostVirtualMachine(
                 """
@@ -49,9 +109,22 @@ class MainActivity : AppCompatActivity() {
             if (program.output != "") {
                 findViewById<ScrollView>(R.id.scrollView).visibility = View.VISIBLE
                 outCloseBtn.visibility = View.VISIBLE
-                findViewById<TextView>(R.id.output).text = program.output
+                //findViewById<TextView>(R.id.output).text = program.output
+                var str = ""
+                for (i in 0 until recyclerviewAdapter.arrayItems.size) {
+                    str += recyclerviewAdapter.arrayItems[i].number
+                }
+
+                binding.output.text = str
             }
         }
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    private fun init() {
+        binding.buttonAdd.setOnClickListener {
+            val rnds = (0..10).random()
+            recyclerviewAdapter.addBlocks(rnds)
+        }
+    }
 }
