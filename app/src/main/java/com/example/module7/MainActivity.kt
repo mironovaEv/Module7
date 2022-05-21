4e142200b77dd58de33c052303832dbc187abdd8
EmilKirichenko <kirichienko.emil@gmail.com>
2022-05-21 10:21:22 +0700

    Форматирование выражений

--- app/src/main/java/com/example/module7/MainActivity.kt
+++ app/src/main/java/com/example/module7/MainActivity.kt
@@ -97,6 +97,7 @@ class MainActivity : AppCompatActivity() {
             console.visibility = View.VISIBLE
             consoleClose.visibility = View.VISIBLE
             binding.buttonAdd.visibility = View.GONE
+            buttonRun.visibility = View.GONE
             when (expectingInput) {
                 null -> {
                     buttonRestart.visibility = View.GONE
@@ -253,6 +254,7 @@ class MainActivity : AppCompatActivity() {
             buttonResume.visibility = View.GONE
             buttonRestart.visibility = View.GONE
             binding.buttonAdd.visibility = View.VISIBLE
+            buttonRun.visibility = View.VISIBLE
         }
 
         consoleOpen.setOnClickListener {
